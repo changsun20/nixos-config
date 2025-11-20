2809ee530dd7bc2de233d713f0319b441bb1e0bb
changsun20 <110759360+changsun20@users.noreply.github.com>
2025-11-19 22:06:40 -0500

    chore: switch to the right timezone

--- base/vm.nix
+++ base/vm.nix
@@ -39,7 +39,7 @@
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Set your time zone.
-  time.timeZone = "Asia/Shanghai";
+  time.timeZone = "America/New_York";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
