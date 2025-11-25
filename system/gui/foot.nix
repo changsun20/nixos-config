a8dcbd077013cf513499f917fef35327338fe36b
changsun20 <110759360+changsun20@users.noreply.github.com>
2025-11-24 22:35:58 -0500

    fix: maximized foot on startup and disable csd

--- system/gui/foot.nix
+++ system/gui/foot.nix
@@ -3,8 +3,15 @@
     enable = true;
     theme = "tokyonight-night";
     settings = {
-      main.font = "JetbrainsMono Nerd Font:size=16";
+      main = {
+        font = "JetbrainsMono Nerd Font:size=16";
+        initial-window-mode = "maximized";
+      };
       scrollback.lines = 10000;
+      csd = {
+        preferred = "none";
+        size = 0;
+      };
     };
   };
 }
