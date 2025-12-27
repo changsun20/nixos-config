8301a4469c1735d789ad3565a756463ba981a4d9
changsun20 <110759360+changsun20@users.noreply.github.com>
2025-12-28 00:19:39 +0800

    feat: set user wide fonts pref

--- home/core/fonts.nix
+++ home/core/fonts.nix
@@ -1,9 +1,15 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    fira-code
-    nerd-fonts.jetbrains-mono
     noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
+    nerd-fonts.jetbrains-mono
   ];
+  fonts.fontconfig = {
+    defaultFonts = {
+      serif = ["Noto Serif" "Noto Serif CJK SC"];
+      sansSerif = ["Noto Sans" "Noto Sans CJK SC"];
+      monospace = ["JetBrainsMono Nerd Font"];
+    };
+  };
 }
